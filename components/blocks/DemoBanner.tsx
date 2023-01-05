8bc1b82e8d6e97cda7cb4d5d24f8d03e25e0bf8b
finnelliott <finn@finnelliott.com>
2023-01-06 00:26:25 +1100

    --

--- components/blocks/DemoBanner.tsx
+++ components/blocks/DemoBanner.tsx
@@ -8,16 +8,16 @@ import SecondaryButton from '../shared/SecondaryButton';
 const DemoBanner = () => {
     const [ isShowing, setIsShowing ] = useState(true)
     return (
-        <div className="bg-primary-50 border-b border-primary-300 sticky top-0 left-0 right-0 z-50">
+        <div className="bg-primary-50 border-b border-primary-200 sticky top-0 left-0 right-0 z-50">
             <div className="mx-auto max-w-7xl py-3 px-3 sm:px-8 lg:px-8">
             <div className="flex items-center justify-between flex-col sm:flex-row">
                 <div className="flex flex-1 items-center py-2">
                 <span className="flex rounded-lg">
-                    <SparklesIcon className="h-6 w-6 text-primary-600" aria-hidden="true" />
+                    <SparklesIcon className="h-6 w-6 text-primary-700" aria-hidden="true" />
                 </span>
                 <p className="flex-1 ml-3 text-sm">
-                    <span className="text-primary-700 font-medium">Does your website look this good?{` `}</span>
-                    <span className="text-primary-600 font-normal">Mistrata provides performance dental websites for just £40/mth.</span>
+                    <span className="text-primary-800 font-medium">Does your website look this good?{` `}</span>
+                    <span className="text-primary-800 font-normal">Mistrata provides performance dental websites for just £40/mth.</span>
                 </p>
                 </div>
                 <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 ml-0 sm:ml-3 sm:w-auto">
