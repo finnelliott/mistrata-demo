37f066f14fb320c740d00729b84619549d7eb07b
finnelliott <finn@finnelliott.com>
2023-01-05 18:26:17 +1100

    --

--- components/shared/PageHead.tsx
+++ components/shared/PageHead.tsx
@@ -3,7 +3,7 @@ import { Blog, Page } from "../../payload-types";
 
 export default function PageHead({ page }: { page: Page | Blog } ) {
     const business = use(fetch(`${process.env.NEXT_PUBLIC_CMS_URL}/api/globals/business`).then((res) => res.json()))
-    const meta_image_query = `title=${page.title}&description=${page.meta.description}&logo=${process.env.NEXT_PUBLIC_CMS_URL+business.logo.url}&business=${business.name}`
+    const meta_image_query = encodeURIComponent(`title=${page.title}&description=${page.meta.description}&logo=${process.env.NEXT_PUBLIC_CMS_URL+business.logo.url}&business=${business.name}`)
 
     return (
         <>
@@ -23,7 +23,7 @@ export default function PageHead({ page }: { page: Page | Blog } ) {
         <meta name="twitter:description" content={page.meta.description} />
 
         {/* <-- Twitter Summary card images must be at least 120x120px --> */}
-        <meta name="twitter:image" content={encodeURIComponent(`${process.env.NEXT_PUBLIC_DOMAIN}/api/og?width=600&height=600&${meta_image_query}`)} />
+        <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/api/og?width=600&height=600&${meta_image_query}`} />
 
         {/* <!-- Open Graph data --> */}
         <meta property="og:title" content={page.meta.title} />
@@ -31,7 +31,7 @@ export default function PageHead({ page }: { page: Page | Blog } ) {
         <meta property="og:site_name" content={business.name} />
         <meta
             property="og:image"
-            content={encodeURIComponent(`${process.env.NEXT_PUBLIC_DOMAIN}/api/og?${meta_image_query}`)}
+            content={`${process.env.NEXT_PUBLIC_DOMAIN}/api/og?${meta_image_query}`}
         />
 
         {/* {page.meta.image?.filename && <meta property="og:image" content={`https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com/images/${page.meta.image.filename}`} />} */}
