66be84e69ba29f3a19ed0856d2ecbd5b47906844
finnelliott <finn@finnelliott.com>
2023-01-23 11:09:02 +0000

    --

--- app/(marketing)/head.tsx
+++ app/(marketing)/head.tsx
@@ -1,12 +1,13 @@
 import { use } from "react"
 import PageHead from "../../components/shared/PageHead"
 import getBusiness from "../../lib/getBusiness"
+import { Business } from "../../payload-types"
 
 export default function Head({ params }: { params: { slug: string } }) {
     const business = use(getBusiness())
     return (
       <>
-      <PageHead business={business} />
+      <PageHead business={business as Business} />
       </>
     )
 }
