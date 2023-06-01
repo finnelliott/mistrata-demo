4905e0ba8ab58d16a88190ab3ac95c2dcd12726e
finnelliott <finn@finnelliott.com>
2023-06-01 17:02:15 +0100

    --

--- components/blocks/BlogPosts.tsx
+++ components/blocks/BlogPosts.tsx
@@ -9,16 +9,12 @@ type Props = {
 
 const BlogPosts = async ({ block }: Props) => {
 
-<<<<<<< HEAD
   const payload = await getPayloadClient();
 
   const { docs: posts } = await payload.find({
     collection: 'blog',
     limit: 1000
   });
-=======
-  const posts = use(fetch(`${process.env.NEXT_PUBLIC_CMS_URL}/api/blog?sort=-createdAt&limit=1000`).then(res => res.json()).then(data => data.docs as Blog[]))
->>>>>>> parent of 5f0fef2 (--)
 
   return (
       <BlogPostsLayout block={block} posts={posts} />
